d25e1db1bbd788bca2bb55d8b0a06081770bf156
Abdallah Bari <abdellah01win@gmail.com>
2022-05-01 22:47:56 +0000

    style order info popup and add icons

--- Components/order_info_popup.php
+++ Components/order_info_popup.php
@@ -7,7 +7,7 @@
             </div>
             <div class="logo">Malibu's</div>
             <h3 class="alert-title">Order Info</h3>
-            <div class="info">
+            <div class="order-info">
                 <?php foreach ($orders as $order) : ?>
                     <?php if ($order['id'] === $orderId) : ?>
                         <?php
@@ -16,24 +16,36 @@
                         $sql = 'SELECT user_name FROM users WHERE id=' . $order['userId'];
                         $result = mysqli_query($conn, $sql);
                         $user = mysqli_fetch_all($result, MYSQLI_ASSOC); ?>
-                        <?php for ($i = 0; $i < count($dishes_in_order); $i++) : ?>
-                            <div class="order-dishes">
-                                <img class="meal-img" src=<?php echo '.' . $dishes_in_order[$i][0]['img_link'] ?> alt="">
-                                <div class="qnt">
-                                    <?php echo json_decode($order['qnt_array'])[$i]; ?>
+                        <div class="order-dishes-container">
+                            <?php for ($i = 0; $i < count($dishes_in_order); $i++) : ?>
+                                <div class="order-dishes">
+                                    <img class="meal-img" src=<?php echo '.' . $dishes_in_order[$i][0]['img_link'] ?> alt="">
+                                    <div class="qnt">
+                                        <?php echo json_decode($order['qnt_array'])[$i]; ?>
+                                    </div>
                                 </div>
-                                <div>
-                                    <?php echo $dishes_in_order[$i][0]['name'] ?>
-                                </div>
-                            </div>
-                        <?php endfor ?>
-                        <?php echo $order['adress'] ?>
-                        <?php echo $order['g_total'] ?>
-                        <?php echo $order['order_date'] ?>
-                        <?php echo $user[0]['user_name'] ?>
+                            <?php endfor ?>
+                        </div>
+                        <div class="info-item">
+                        <img  src="Assets/icons/map-pin-line.svg" alt="">
+                            <?php echo $order['adress'] ?>
+                        </div>
+                        <div class="info-item">
+                        <img  src="Assets/icons/currency-dollar.svg" alt="">
+                            <?php echo $order['g_total'] . ".00" ?>
+                        </div>
+                        <div class="info-item">
+                            <img  src="Assets/icons/user-dark.svg" alt="">
+                            <?php echo $user[0]['user_name'] ?>
+                        </div>
+                        <div class="info-item">
+                        <img  src="Assets/icons/calendar.svg" alt="">
+                            <?php echo substr($order['order_date'], 2, -3)  ?>
+                        </div>
+
                         <div class="btns-container">
-                            <a class="btn-dark" <?php echo "href='app/routes/order_status.php?confirm=" . $order['id'] . "'" ?>>Confirm</a>
-                            <a class="btn-dark" <?php echo "href='app/routes/order_status.php?deny=" . $order['id'] . "'" ?>>Deny</a>
+                            <a class="btn-mid" <?php echo "href='app/routes/order_status.php?confirm=" . $order['id'] . "'" ?>>Confirm</a>
+                            <a class="btn-mid" <?php echo "href='app/routes/order_status.php?deny=" . $order['id'] . "'" ?>>Deny</a>
                         </div>
                     <?php endif ?>
                 <?php endforeach ?>
