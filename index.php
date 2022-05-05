ddf68fb0992935506cbae88a4568ac139e91b215
Abdallah Bari <abdellah01win@gmail.com>
2022-05-05 18:43:11 +0000

    restyle hero section and add partners logos

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $dishes = mysqli_fetch_all($result, MYSQLI_ASSOC);
             include('./Components/whishlist_popup.php');
         }; ?>
         <?php include('./Components/navigation.php'); ?>
-        <div class="content grid-2 container">
+        <div class="content grid-2">
             <div class="hero-text">
                 <h1 class="header">Quality food, locally sourced, and baeutifly cooked</h1>
                 <p class="paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam vitae obcaecati placeat at voluptate, molestias non dolorum.</p>
@@ -28,26 +28,24 @@ $dishes = mysqli_fetch_all($result, MYSQLI_ASSOC);
             </div>
             <div class="hero-img">
                 <div class="img-container">
-                    <img src="./Assets/hero-1.jpg" alt="">
+                    <img src="./Assets/hero-80.jpg" alt="">
                 </div>
             </div>
         </div>
     </section>
 
 
-    <!-- <section class="partners">
+    <section class="partners">
     <div id="partners" class="container">
-            <h2 class="title">Join our world-class partners</h2>
-
             <ul class="partner-list">
                 <li><img src="./Assets/Partners/Daco_4302912.png" alt="TechCrunch"></li>
                 <li> <img src="./Assets/Partners/PngItem_981502.png" alt="Business Insider"> </li>
                 <li><img src="./Assets/Partners/Daco_6044851.png" alt="The New Yourk Times"></li>
-                <li> <img src="./Assets/Partners/Daco_4638935.png" alt="Forbs"></li>
-                <li><img src="./Assets/Partners/PngItem_245292.png" alt="USA Today Network"></li>
+                <!-- <li> <img src="./Assets/Partners/Daco_4638935.png" alt="Forbs"></li> -->
+                <li><img src="./Assets/Partners/PngItem_245292.png" alt="Gazprom"></li>
             </ul>
         </div>
-    </section> -->
+    </section>
 
     <!-- <section class="about">
         <div class="grid-2 container">
