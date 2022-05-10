89085d9069d4bfcb33c5049b783957a3663409d4
Abdallah Bari <abdellah01win@gmail.com>
2022-05-10 23:00:29 +0100

    modify about page chefs' section

--- about.php
+++ about.php
@@ -8,9 +8,16 @@
     <section class="">
         <div class="about-hero">
             <div class="about-text">
-                <div class="title">Lorem, ipsum dolor.</div>
-                <div class="dark-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim autem iure dignissimos possimus eos ab molestiae rerum minus voluptate?</div>
-                <div class="dark-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim autem iure dignissimos possimus eos ab molestiae rerum minus voluptate?</div>
+                <div class="title">About Malibu's</div>
+                <div class="dark-paragraph">
+                    Out of love for the art of cuisne, our goal is to bring the joy and tast of food into the life of our comunity of costumers.
+                </div>
+                <div class="dark-paragraph">
+
+                    
+
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis enim autem iure dignissimos possimus eos ab molestiae rerum minus voluptate?
+                </div>
             </div>
     </section>
     <section class="container stats">
@@ -21,32 +28,32 @@
                 <div class="dark-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, eligendi.</div>
             </div>
             <div>
-                <div class="stat">+45K</div>
-                <div class="stat-title">Satisfied customers</div>
+                <div class="stat">+1000</div>
+                <div class="stat-title">5 Star reviews</div>
                 <div class="dark-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, eligendi.</div>
             </div>
             <div>
-                <div class="stat">+1000</div>
-                <div class="stat-title">5 Star reviews</div>
+                <div class="stat">17</div>
+                <div class="stat-title">Cretics' choise awards</div>
                 <div class="dark-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, eligendi.</div>
             </div>
         </div>
     </section>
     <section class="chefs">
-        <div class="title container">We have the world's best chefs!</div>
+        <div class="title container">Our world-class Chefs need no introduction.</div>
         <div class="container grid-3">
-        <div class="chef">
-            <img class="chef-img" src="Assets/chef-1.jpg" alt="">
-            <div class="chef-name">Chrestina Bale</div>
-        </div>
-        <div class="chef">
-            <img class="chef-img" src="Assets/chef-2.jpg" alt="">
-            <div class="chef-name">Christopher Nolan</div>
-        </div>
-        <div class="chef">
-            <img class="chef-img" src="Assets/chef-3.jpg" alt="">
-            <div class="chef-name">Tom Harrdy</div>
-        </div>
+            <div class="chef">
+                <img class="chef-img" src="Assets/chef-1.jpg" alt="">
+                <div class="chef-name">Chrestina Bale</div>
+            </div>
+            <div class="chef">
+                <img class="chef-img" src="Assets/chef-2.jpg" alt="">
+                <div class="chef-name">Christopher Nolan</div>
+            </div>
+            <div class="chef">
+                <img class="chef-img" src="Assets/chef-3.jpg" alt="">
+                <div class="chef-name">Tom Harrdy</div>
+            </div>
         </div>
     </section>
 
